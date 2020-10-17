a5ddfd3920c4f9323c283d8e66ee229b119ff823
yoonseungjae <tmdwo5811@gmail.com>
2020-10-17 15:50:39 +0900

    git action test

--- repositories/user.repository.js
+++ repositories/user.repository.js
@@ -3,6 +3,7 @@ const userModel = require('../models/user.model');
 exports.create = async (query) => {
     try {
         const result = await userModel.create([query]);
+        
         return result;
     } catch (e) {
         console.log(e);
