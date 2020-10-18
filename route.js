d158c0f764eec2143f4b7ca27be0912c048865c3
yoonseungjae <tmdwo5811@gmail.com>
2020-10-18 20:54:10 +0900

    배포 자동화 테스트 2

--- route.js
+++ route.js
@@ -5,9 +5,6 @@ module.exports = (router) => {
     router.route("/hello")
     .get(userController.hello);
 
-    router.route("/hello2")
-    .get(userController.hello);
-
     router.route("/create/message")
     .get(userController.createMessage);
 
